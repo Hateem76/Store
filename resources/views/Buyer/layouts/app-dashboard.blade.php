7af1c7e0196da0bd274ff428f7c763ac1a83d9a6
Muhammad Hateem <m8246767@gmail.com>
2022-03-04 16:04:59 +0500

    hey

--- resources/views/Buyer/layouts/app-dashboard.blade.php
+++ resources/views/Buyer/layouts/app-dashboard.blade.php
@@ -30,7 +30,7 @@
                 <label for="nav-toggle">
                 <span class="fas fa-bars mr-3 ml-2"></span>
                 </label>
-                Buyer Group of Industries
+                Buyer Group offff Industries
             </h3>
             <div class="search-wrapper">
                 <!-- <span class="fas fa-search"></span> -->
