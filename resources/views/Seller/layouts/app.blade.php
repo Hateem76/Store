b7445abe11393f8774e746f911b46dfc2baf7c71
Muhammad Hateem <m8246767@gmail.com>
2022-03-08 16:09:42 +0500

    Products

--- resources/views/Seller/layouts/app.blade.php
+++ resources/views/Seller/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
         <link rel="stylesheet" href="{{ asset('css/sidebar2.css') }}">
         <link rel="stylesheet" href="{{ asset('css/card.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/buyer/tender_table.css') }}">
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
             integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
