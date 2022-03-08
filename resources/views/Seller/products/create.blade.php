6a6b75b360f668ada1485b8e65b64fed607d993d
Muhammad Hateem <m8246767@gmail.com>
2022-03-08 16:04:30 +0500

    Products

--- resources/views/Seller/products/create.blade.php
+++ resources/views/Seller/products/create.blade.php
@@ -125,7 +125,7 @@
     <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <link rel="stylesheet" href="{{ asset('css/card.css') }}">
     <link rel="stylesheet" href="{{ asset('css/product.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/buyer/tender_table.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/buyer/tender_table_for_product.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script type="text/javascript">
@@ -166,7 +166,7 @@
 
 
                         <div class="container-fluid add-product-container shadow-lg mt-4"
-                        style="background-color: white; width:72%; height:auto; border-radius:10px;">
+                        style="background-color: white; width:100%; height:auto; border-radius:10px;">
 
                           
                             <div class="row mt-2">
@@ -270,7 +270,7 @@
                                     </div>
                                 </div>
                                 <div class="row mx-auto justify-content-center mt-4">
-                                    <button type="submit" class="btn btn-dark btn-lg mt-4" style="width: 300px;">ADD</button>
+                                    <button type="submit" class="btn btn-dark btn-lg mt-4 mb-4" style="width: 300px;">ADD</button>
                                 </div>
                             </form>
                         </div>
